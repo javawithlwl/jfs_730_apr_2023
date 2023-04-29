e2bd1afd53038736c3ce40683ada658c386811d1
javawithlwl <javawithlwl@gmail.com>
2023-04-29 09:08:06 +0530

    Added String and Break examples

---
+++ src/main/java/com/careerit/jfs/cj/day11/StringBufferExample.java
@@ -0,0 +1,12 @@
+package com.careerit.jfs.cj.day11;
+
+public class StringBufferExample {
+
+  public static void main(String[] args) {
+
+    StringBuilder sb1 = new StringBuilder("Hello");
+    StringBuilder sb2 = new StringBuilder("Hello");
+    System.out.println(sb1 == sb2);
+    System.out.println(sb1.toString().equals(sb2.toString()));
+  }
+}
--- src/main/java/com/careerit/jfs/cj/day4/BreakAndContinueExample.java
+++ src/main/java/com/careerit/jfs/cj/day4/BreakAndContinueExample.java
@@ -1,4 +1,4 @@
-package com.careerit.jfs.cj.day4;
+
 
 public class BreakAndContinueExample {
 
