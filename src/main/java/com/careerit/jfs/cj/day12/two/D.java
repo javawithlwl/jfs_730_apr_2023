cacd300660567fa495cce3954c3064704f663112
javawithlwl <javawithlwl@gmail.com>
2023-05-02 08:55:08 +0530

    Added object oriented examples

---
+++ src/main/java/com/careerit/jfs/cj/day12/two/D.java
@@ -0,0 +1,14 @@
+package com.careerit.jfs.cj.day12.two;
+
+import com.careerit.jfs.cj.day12.one.A;
+
+public class D extends A {
+
+  @Override
+  public void showDetails() {
+    //System.out.println(pri_a);
+     System.out.println(pub_a);
+     System.out.println(pro_a);
+    //System.out.println(def_a);
+  }
+}
---
+++ src/main/java/com/careerit/jfs/cj/day12/two/E.java
@@ -0,0 +1,15 @@
+package com.careerit.jfs.cj.day12.two;
+
+import com.careerit.jfs.cj.day12.one.A;
+
+public class E {
+
+  public void showDetails() {
+    A obj = new A();
+    //System.out.println(obj.pri_a);
+    System.out.println(obj.pub_a);
+    // System.out.println(obj.pro_a);
+    // System.out.println(obj.def_a);
+
+  }
+}
